c78a905eace1975b874356968a123ddc39459976
Martin Smeder <119725728+martinsmeder@users.noreply.github.com>
2023-07-05 21:28:54 +0200

    Added tests for ShipFactory and GameboardFactory

--- src/__tests__/app-logic.test.js
+++ src/__tests__/app-logic.test.js
@@ -1,13 +1,100 @@
-/* global describe, test, expect */
+/* global describe, it, expect, beforeEach */
 
-import addNumbers from "../app-logic";
+import { ShipFactory, GameboardFactory } from "../app-logic";
 
-describe("addNumbers", () => {
-  test("adds two numbers correctly", () => {
-    expect(addNumbers(2, 3)).toBe(5);
+describe("ShipFactory", () => {
+  let ship;
+
+  beforeEach(() => {
+    ship = ShipFactory(3);
   });
 
-  test("returns zero when adding zero to a number", () => {
-    expect(addNumbers(7, 0)).toBe(7);
+  it("should have the correct length", () => {
+    // Incoming query: Asserting the expected output
+    expect(ship.length).toBe(3);
+  });
+
+  it("should have initial hit count of 0", () => {
+    // Incoming query: Asserting the expected output
+    expect(ship.isSunk()).toBe(false);
+  });
+
+  it("should increase hit count when hit() is called", () => {
+    // Incoming command: Asserting the direct public side effect caused by the command
+    ship.hit();
+    expect(ship.isSunk()).toBe(false);
+
+    ship.hit();
+    expect(ship.isSunk()).toBe(false);
+
+    ship.hit();
+    expect(ship.isSunk()).toBe(true);
+  });
+
+  it("should be considered sunk when hit count equals length", () => {
+    // Incoming query: Asserting the expected output
+    expect(ship.isSunk()).toBe(false);
+
+    ship.hit();
+    expect(ship.isSunk()).toBe(false);
+
+    ship.hit();
+    expect(ship.isSunk()).toBe(false);
+
+    ship.hit();
+    expect(ship.isSunk()).toBe(true);
+  });
+});
+
+describe("GameboardFactory", () => {
+  let gameboard;
+
+  beforeEach(() => {
+    gameboard = GameboardFactory();
+  });
+
+  it("should be able to place ships at specific coordinates", () => {
+    const ship = ShipFactory(3);
+    gameboard.placeShip(ship, 0, 0, "horizontal");
+
+    // Assert that the ship is placed correctly at the specified coordinates
+    expect(gameboard.grid[0][0]).toEqual(ship);
+    expect(gameboard.grid[0][1]).toEqual(ship);
+    expect(gameboard.grid[0][2]).toEqual(ship);
+  });
+
+  it("should record missed shot coordinates when attack misses", () => {
+    gameboard.receiveAttack(0, 0);
+
+    // Assert that the missed shot coordinates are recorded
+    expect(gameboard.missedAttacks).toContainEqual([0, 0]);
+  });
+
+  it("should send 'hit' function to the correct ship when attack hits", () => {
+    const ship = ShipFactory(3);
+    const placedShip = gameboard.placeShip(ship, 0, 0, "horizontal"); // Assign the returned ship object
+
+    gameboard.receiveAttack(0, 0);
+
+    // Assert that the 'hit' function is sent to the correct ship
+    expect(placedShip.hits).toBe(1);
+  });
+
+  it("should be able to report whether all ships have been sunk", () => {
+    const ship1 = ShipFactory(3);
+    const ship2 = ShipFactory(2);
+
+    gameboard.placeShip(ship1, 0, 0, "horizontal");
+    gameboard.placeShip(ship2, 1, 0, "vertical");
+
+    // Attack all the positions of the ships
+    gameboard.receiveAttack(0, 0);
+    gameboard.receiveAttack(0, 1);
+    gameboard.receiveAttack(0, 2);
+    gameboard.receiveAttack(1, 0);
+    gameboard.receiveAttack(2, 0);
+
+    // Assert that all ships are reported as sunk
+    expect(gameboard.allShipsSunk()).toBe(true);
   });
 });
--- src/app-logic.js
+++ src/app-logic.js
@@ -1,7 +1,90 @@
 console.log("this also seem to be working");
 
-function addNumbers(a, b) {
-  return a + b;
-}
+export const ShipFactory = (length) => {
+  let hits = 0;
 
-export default addNumbers;
+  function hit() {
+    hits += 1;
+  }
+
+  function isSunk() {
+    return hits === length;
+  }
+
+  return {
+    length,
+    hits,
+    hit,
+    isSunk,
+  };
+};
+
+export const GameboardFactory = () => {
+  const grid = Array(10)
+    .fill(null)
+    .map(() => Array(10).fill(null));
+
+  const missedAttacks = [];
+
+  function placeShip(ship, x, y, direction) {
+    const shipLength = ship.length;
+
+    // Check if the ship can be placed within the grid and direction is valid
+    if (x < 0 || x >= 10 || y < 0 || y >= 10) {
+      throw new Error(
+        "Invalid coordinates. Ship cannot be placed outside the grid."
+      );
+    }
+
+    if (direction === "horizontal" && y + shipLength > 10) {
+      throw new Error(
+        "Invalid coordinates. Ship does not fit within the grid horizontally."
+      );
+    }
+
+    if (direction === "vertical" && x + shipLength > 10) {
+      throw new Error(
+        "Invalid coordinates. Ship does not fit within the grid vertically."
+      );
+    }
+
+    // Place the ship in the grid
+    for (let i = 0; i < shipLength; i += 1) {
+      if (direction === "horizontal") {
+        grid[x][y + i] = ship;
+      } else if (direction === "vertical") {
+        grid[x + i][y] = ship;
+      }
+    }
+
+    return ship; // Return the ship object after placing it on the grid
+  }
+
+  function receiveAttack(x, y) {
+    if (x < 0 || x >= 10 || y < 0 || y >= 10) {
+      throw new Error("Invalid coordinates. Attack must be within the grid.");
+    }
+
+    const attackedShip = grid[x][y];
+
+    if (attackedShip === null) {
+      missedAttacks.push([x, y]);
+    } else {
+      attackedShip.hit();
+    }
+  }
+
+  function allShipsSunk() {
+    return grid.every((row) =>
+      row.every((cell) => cell === null || cell.isSunk())
+    );
+  }
+
+  return {
+    grid,
+    missedAttacks,
+    placeShip,
+    receiveAttack,
+    allShipsSunk,
+  };
+};
