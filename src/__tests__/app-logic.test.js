9ccb38a06a9af386fc5402cb02c380830f750d42
Martin Smeder <119725728+martinsmeder@users.noreply.github.com>
2023-07-06 21:41:38 +0200

    Removed GameboardFactory(), refactored ShipFactory()

--- src/__tests__/app-logic.test.js
+++ src/__tests__/app-logic.test.js
@@ -3,98 +3,30 @@
 import { ShipFactory, GameboardFactory } from "../app-logic";
 
 describe("ShipFactory", () => {
-  let ship;
+  const ship = ShipFactory(3);
 
-  beforeEach(() => {
-    ship = ShipFactory(3);
-  });
-
-  it("should have the correct length", () => {
-    // Incoming query: Asserting the expected output
+  it("should create a ship object with the correct length", () => {
     expect(ship.length).toBe(3);
   });
 
-  it("should have initial hit count of 0", () => {
-    // Incoming query: Asserting the expected output
+  it("should create a ship object with initial hits count of 0", () => {
+    expect(ship.hits).toBe(0);
+  });
+
+  it("hit() method should increase the hits count", () => {
+    ship.hit();
+    expect(ship.hits).toBe(1);
+  });
+
+  it("isSunk() method should return false when hits count is less than length", () => {
+    ship.hit();
     expect(ship.isSunk()).toBe(false);
   });
 
-  it("should increase hit count when hit() is called", () => {
-    // Incoming command: Asserting the direct public side effect caused by the command
+  it("isSunk() method should return true when hits count equals length", () => {
     ship.hit();
-    expect(ship.isSunk()).toBe(false);
-
     ship.hit();
-    expect(ship.isSunk()).toBe(false);
-
-    ship.hit();
-    expect(ship.isSunk()).toBe(true);
-  });
-
-  it("should be considered sunk when hit count equals length", () => {
-    // Incoming query: Asserting the expected output
-    expect(ship.isSunk()).toBe(false);
-
-    ship.hit();
-    expect(ship.isSunk()).toBe(false);
-
-    ship.hit();
-    expect(ship.isSunk()).toBe(false);
-
     ship.hit();
     expect(ship.isSunk()).toBe(true);
-  });
-});
-
-describe("GameboardFactory", () => {
-  let gameboard;
-
-  beforeEach(() => {
-    gameboard = GameboardFactory();
-  });
-
-  it("should be able to place ships at specific coordinates", () => {
-    const ship = ShipFactory(3);
-    gameboard.placeShip(ship, 0, 0, "horizontal");
-
-    // Assert that the ship is placed correctly at the specified coordinates
-    expect(gameboard.grid[0][0]).toEqual(ship);
-    expect(gameboard.grid[0][1]).toEqual(ship);
-    expect(gameboard.grid[0][2]).toEqual(ship);
-  });
-
-  it("should record missed shot coordinates when attack misses", () => {
-    gameboard.receiveAttack(0, 0);
-
-    // Assert that the missed shot coordinates are recorded
-    expect(gameboard.missedAttacks).toContainEqual([0, 0]);
-  });
-
-  it("should send 'hit' function to the correct ship when attack hits", () => {
-    const ship = ShipFactory(3);
-    const placedShip = gameboard.placeShip(ship, 0, 0, "horizontal"); // Assign the returned ship object
-
-    gameboard.receiveAttack(0, 0);
-
-    // Assert that the 'hit' function is sent to the correct ship
-    expect(placedShip.hits).toBe(1);
-  });
-
-  it("should be able to report whether all ships have been sunk", () => {
-    const ship1 = ShipFactory(3);
-    const ship2 = ShipFactory(2);
-
-    gameboard.placeShip(ship1, 0, 0, "horizontal");
-    gameboard.placeShip(ship2, 1, 0, "vertical");
-
-    // Attack all the positions of the ships
-    gameboard.receiveAttack(0, 0);
-    gameboard.receiveAttack(0, 1);
-    gameboard.receiveAttack(0, 2);
-    gameboard.receiveAttack(1, 0);
-    gameboard.receiveAttack(2, 0);
-
-    // Assert that all ships are reported as sunk
-    expect(gameboard.allShipsSunk()).toBe(true);
   });
 });
--- src/app-logic.js
+++ src/app-logic.js
@@ -1,90 +1,23 @@
-console.log("this also seem to be working");
-
+// eslint-disable-next-line import/prefer-default-export
 export const ShipFactory = (length) => {
   let hits = 0;
 
-  function hit() {
+  const hit = () => {
     hits += 1;
-  }
+  };
 
-  function isSunk() {
-    return hits === length;
-  }
+  const isSunk = () => hits >= length; // Return true if hits is equal to or greater than length
 
   return {
     length,
-    hits,
     hit,
     isSunk,
-  };
-};
-
-export const GameboardFactory = () => {
-  const grid = Array(10)
-    .fill(null)
-    .map(() => Array(10).fill(null));
-
-  const missedAttacks = [];
-
-  function placeShip(ship, x, y, direction) {
-    const shipLength = ship.length;
-
-    // Check if the ship can be placed within the grid and direction is valid
-    if (x < 0 || x >= 10 || y < 0 || y >= 10) {
-      throw new Error(
-        "Invalid coordinates. Ship cannot be placed outside the grid."
-      );
-    }
-
-    if (direction === "horizontal" && y + shipLength > 10) {
-      throw new Error(
-        "Invalid coordinates. Ship does not fit within the grid horizontally."
-      );
-    }
-
-    if (direction === "vertical" && x + shipLength > 10) {
-      throw new Error(
-        "Invalid coordinates. Ship does not fit within the grid vertically."
-      );
-    }
-
-    // Place the ship in the grid
-    for (let i = 0; i < shipLength; i += 1) {
-      if (direction === "horizontal") {
-        grid[x][y + i] = ship;
-      } else if (direction === "vertical") {
-        grid[x + i][y] = ship;
-      }
-    }
-
-    return ship; // Return the ship object after placing it on the grid
-  }
-
-  function receiveAttack(x, y) {
-    if (x < 0 || x >= 10 || y < 0 || y >= 10) {
-      throw new Error("Invalid coordinates. Attack must be within the grid.");
-    }
-
-    const attackedShip = grid[x][y];
-
-    if (attackedShip === null) {
-      missedAttacks.push([x, y]);
-    } else {
-      attackedShip.hit();
-    }
-  }
-
-  function allShipsSunk() {
-    return grid.every((row) =>
-      row.every((cell) => cell === null || cell.isSunk())
-    );
-  }
-
-  return {
-    grid,
-    missedAttacks,
-    placeShip,
-    receiveAttack,
-    allShipsSunk,
+    // Getter function for hits. Using function declaration to ensure it has access to the
+    // `hits` variable dynamically, since function declarations are hoisted and can be used
+    //  before they are defined. This allows accessing the current value of `hits` even before
+    // calling the `hit` function
+    get hits() {
+      return hits;
+    },
   };
 };
